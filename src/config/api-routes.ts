6a1f98adb2f8578b157fb937d0e3c51a765d66a3
David Bašić <d.basic97.db@gmail.com>
2023-06-08 20:34:59 +0200

    slight change

--- src/config/api-routes.ts
+++ src/config/api-routes.ts
@@ -1,9 +1,11 @@
 export const routesParams = {
-    VALIDATION_API: "http://localhost:8081",
-}
+	VALIDATION_API: "http://localhost:8081",
+};
 
 const api_routes = {
-    ROUTE_VALIDATION_HOME: `${routesParams.VALIDATION_API}`,
+	ROUTE_VALIDATION_HOME: `${routesParams.VALIDATION_API}`,
 
-    ROUTE_VALIDATION_XSD: `${routesParams.VALIDATION_API}/validators/xsdValidation`
-}
+	ROUTE_VALIDATION_XSD: `${routesParams.VALIDATION_API}/validators/xsdValidation`,
+};
+
+export default api_routes;
