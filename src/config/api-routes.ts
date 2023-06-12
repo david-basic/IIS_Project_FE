0fe5bddb38443a9f893b3adcbd5db31b885c4844
David Bašić <d.basic97.db@gmail.com>
2023-06-12 22:27:51 +0200

    RNG done

--- src/config/api-routes.ts
+++ src/config/api-routes.ts
@@ -3,9 +3,9 @@ export const routesParams = {
 };
 
 const api_routes = {
-
 	ROUTE_VALIDATION_XSD: `${routesParams.VALIDATION_API}/validators/xsdValidation`,
 	ROUTE_UPLOAD_FILE: `${routesParams.VALIDATION_API}/validators/fileUpload`,
+	ROUTE_VALIDATION_RNG: `${routesParams.VALIDATION_API}/validators/rngValidation`,
 };
 
 export default api_routes;
