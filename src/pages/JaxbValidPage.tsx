9a208d0233b95ea156a7373e1ade299a0f9f609e
David Bašić <d.basic97.db@gmail.com>
2023-06-19 21:13:01 +0200

    jaxb validates a server file not SOAP file

--- src/pages/JaxbValidPage.tsx
+++ src/pages/JaxbValidPage.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { useState } from "react";
+import useHttp from "../hooks/use-http";
+import api_routes from "../config/api-routes";
 
 const JaxbValidPage = () => {
-  return (
-    <div>JaxbValidPage</div>
-  )
-}
+	const [fileValidationResult, setFileValidationResult] = useState<any>();
+	const { sendRequest: validateFileRequest } = useHttp();
 
-export default JaxbValidPage
+	const validationHandler = () => {
+		const manageResponse = (responseData: any) => {
+			setFileValidationResult(responseData.validationResult.toString());
+		};
+
+		validateFileRequest(
+			{
+				url: api_routes.ROUTE_VALIDATION_JAXB,
+			},
+			manageResponse.bind(null)
+		);
+	};
+
+	return (
+		<div className='container-fluid'>
+			<h1>JAXB validation Page</h1>
+			<hr />
+			<div className='row'>
+				<div className='col'>
+					<div className='row'>
+						<h3>Server file validation result:</h3>
+						<div className='alert'>{fileValidationResult}</div>
+					</div>
+					<div className='row mt-2'>
+						<button onClick={validationHandler} className='w-25'>
+							Validate
+						</button>
+					</div>
+				</div>
+
+				<div className='vr'></div>
+
+				<div className='col'>
+					<div className='row'></div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default JaxbValidPage;
